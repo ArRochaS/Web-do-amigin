24f1460224d693373cbb111f898ca635e1c849c8
ArRochaS <118935646+ArRochaS@users.noreply.github.com>
2022-11-25 09:54:12 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+        <title>Hall dos Jogadores</title>
+    </head>
+    <body>
+        <div class='fundo'>
+           <center><h1>O Hall</h1></center><br>
+        </div>
+        <aside>
+            <a href="vizualizar.php"><button type="button">Vizualize um Jogador</button></a><br>
+            <a href="incluir.php"><button type="button">Insira um Jogador</button></a><br>
+            <a href="editar.php"><button type="button">Edite um Jogador</button></a><br>
+            <a href="excluir.php"><button type="button">Apague um Jogador</button></a><br>
+        </aside>
+        <footer>
+            <center><p>o pé kk</p></center>
+        </footer>
+        <?php
+        ?>
+    </body>
+</html>
